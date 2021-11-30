dc7cef124cd0a0d15bd950449263e4b06584d2e8
Adam Hinton <adam.d.hinton@gmail.com>
2021-11-30 15:54:59 -0500

    done making DOM, haven't styled yet, that can come later if I have time

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   render() {
-    return(<div>
-      Github Card
+    return(<div className='container'>
+      <h1>MY GITHUB INFO</h1>
+      <form>
+        <input></input>
+        <button>Search Github Handle</button>
+      </form>
+      
+      <section className='user'>
+        <img src='https://unsplash.com/photos/XChzBEI6x50'/>
+        <h3>Name:</h3>
+        <p>Handle: </p>
+        <p>Followers Count::</p>
+      </section>
+
+      <section className='followers:'>
+        blahblah
+      </section>
     </div>);
   }
 }
